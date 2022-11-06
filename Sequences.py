4f254a15f4b511df210e7ceb46ab4ab5b34566bc
Kasamba Lumwagi <91837493+Kasambx@users.noreply.github.com>
2022-11-06 17:03:00 +0000

    Add files via upload

---
+++ Sequences.py
@@ -0,0 +1,34 @@
+#sequences
+# len()
+list = [1,0,3,4,5,6,7,8,9]
+print(len(list))
+
+#all(s)
+print(all(list))
+
+#any(s)
+print(any(list))
+
+#------operations-----
+# s + r
+s= "window"
+r = "get" 
+n = 3
+x = 30
+y = [50,60,90]
+print(s + r)
+
+print(x * n)
+
+# s[i]
+print(s[0])
+
+#s[i:j:stride]
+print(s[0:4:2])
+
+# x in s
+x in y 
+print(x in y)
+
+# x not in s
+print(x not in y)
